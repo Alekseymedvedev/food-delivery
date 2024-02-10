096af2be6f96c2ba73580445f20fff6f00bce3ac
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-02-10 15:28:16 +0300

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,15 @@ import './reset.scss';
 import './global.scss';
 import {Route, Routes} from "react-router-dom";
 import {routes} from "./routes/routes";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
+    const {tg} = useTelegram()
+    tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+    tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+    tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+    tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+    tg.MainButton.setParams({"color": "#143F6B"});
     return (
         <Suspense fallback={'Загрузка'}>
             <Routes>
