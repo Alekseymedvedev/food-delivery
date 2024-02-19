1294dc21a8586eb633f92a682fedf242a7a2ab24
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-02-19 19:18:51 +0300

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,8 @@ import {useAppDispatch, useAppSelector} from "./hooks/useRedux";
 import {fetchUser} from "./store/slice/userSlice";
 import {adminRoutes, routes} from "./routes/routes";
 import {useAuthUserMutation} from "./store/API/userApi";
-
+// @ts-ignore
+const tg = window.Telegram.WebApp;
 const dataUser = {
     chatId: 1035451470,
     username: "Amed",
@@ -19,14 +20,15 @@ interface IRoutes {
 }
 
 function App() {
-    const {tg} = useTelegram();
+     const { chatId, username } = tg.initDataUnsafe?.user || { chatId: null, username: null };
+    // const {tg} = useTelegram();
 const [disabled,setDisabled]=useState(false)
     const dispatch = useAppDispatch();
     const {user} = useAppSelector((state) => state.userReducer);
     const [allRoutes, setAllRoutes] = useState<IRoutes[]>();
     const [authUser, {data, error}] = useAuthUserMutation()
     useEffect(() => {
-        if(!disabled) authUser(dataUser)
+        if(!disabled) authUser({chatId,username})
         return () => setDisabled(true)
     }, []);
     useEffect(() => {
