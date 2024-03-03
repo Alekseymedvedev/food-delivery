0b6fc2266494b74ef409727f84406a8ce6a2aa27
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-03 11:44:33 +0300

    commit

---
+++ client/src/pages/updateRoleUser/updateRoleUserPage.tsx
@@ -0,0 +1,13 @@
+
+import {MainLayout} from "../../layout/mainLayout"
+
+
+const UpdateRoleUserPage = () => {
+
+    return (
+        <MainLayout heading={''}>
+            <div></div>
+        </MainLayout>
+    );
+};
+export default UpdateRoleUserPage;
