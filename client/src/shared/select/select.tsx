6fe8b89efa9e8e4b1f637140fb8626efb599a13d
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-05 23:43:06 +0300

    commit

--- client/src/shared/select/select.tsx
+++ client/src/shared/select/select.tsx
@@ -6,13 +6,14 @@ import classes from './select.module.scss'
 interface IType{
     onChange: (e:any)=>void
     dataOption: number[] | string[]
+    defaultValue?: string
 }
 
-export const Select: FC<IType> = memo(({onChange,dataOption}) => {
+export const Select: FC<IType> = memo(({onChange,dataOption,defaultValue}) => {
     return (
         <select className={classes.select} onChange={(e)=>onChange(e.target.value)}>
             {dataOption.map((item,index) => (
-                <option key={item} value={item}>{item}</option>
+                <option key={item} value={item} selected={item === defaultValue}>{item}</option>
             ))}
         </select>
     )
