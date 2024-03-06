f6033d0247975a802bfea0f68f12dacf6a48540e
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-06 11:43:56 +0300

    commit

--- client/src/entities/modal/modal.tsx
+++ client/src/entities/modal/modal.tsx
@@ -1,6 +1,7 @@
 import React, {FC, memo} from "react";
 import classes from './modal.module.scss'
 import {Button} from "../../shared/button/button";
+import {useTelegram} from "../../hooks/useTelegram";
 
 
 interface IType {
@@ -11,8 +12,9 @@ interface IType {
 }
 
 export const Modal: FC<IType> = memo(({textModal, textBtn, onClick, error}) => {
+    const {tg} = useTelegram();
     return (
-        <div className={classes.modal}>
+        <div className={tg?.colorScheme !== 'light' ? classes.modal: `${classes.modal} container`}>
             <div className={classes.overlay}></div>
             <div className={error ? `${classes.modalContent} ${classes.error}` : classes.modalContent}>
                 <div className={classes.text}>{textModal}</div>
