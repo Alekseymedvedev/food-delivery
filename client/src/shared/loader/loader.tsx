57ce14d9ca67b36470703ef36390d1d9a8afd081
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-06 11:36:02 +0300

    commit

--- client/src/shared/loader/loader.tsx
+++ client/src/shared/loader/loader.tsx
@@ -15,7 +15,7 @@ export const Loader: FC<IType> = memo(({height, circle}) => {
             circle ?
                 <div className={'loader'}>
                     <ContentLoader viewBox="0 0 400 160" height={'100%'} width={'100%'} foregroundColor="#8E8E8E"
-                                   backgroundColor="#fff">
+                                   backgroundColor="#DEDEDE">
                     <circle cx="150" cy="86" r="8"/>
                     <circle cx="194" cy="86" r="8"/>
                     <circle cx="238" cy="86" r="8"/>
