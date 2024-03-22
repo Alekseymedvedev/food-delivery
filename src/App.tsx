871f73e634bb0abdff69bca8a42fae99f673dfd5
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-22 16:55:00 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,16 +26,16 @@ function App() {
     const [allRoutes, setAllRoutes] = useState<IRoutes[]>();
     const [authUser, {data, error}] = useAuthUserMutation()
     useEffect(() => {
+         // authUser({
+         //     chatId: 1035451470,
+         //     username: 'Amed152',
+         //     queryId: 'AAFOvLc9AAAAAE68tz3ynEhS'
+         // })
          authUser({
-             chatId: 1035451470,
-             username: 'Amed152',
-             queryId: 'AAFOvLc9AAAAAE68tz3ynEhS'
-         })
-        //  authUser({
-        //     chatId: tg?.initDataUnsafe?.user?.id,
-        //     username: tg?.initDataUnsafe?.user?.username,
-        //     queryId: tg?.initDataUnsafe?.query_id
-        // })
+            chatId: tg?.initDataUnsafe?.user?.id,
+            username: tg?.initDataUnsafe?.user?.username,
+            queryId: tg?.initDataUnsafe?.query_id
+        })
     }, []);
     useEffect(() => {
         if (data) {
