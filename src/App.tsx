f8b1d8f3abd04f41fa84280eed57a046dbec1316
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-27 15:26:57 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -27,16 +27,16 @@ function App() {
     const [authUser, {data, error}] = useAuthUserMutation()
 
     useEffect(() => {
-        authUser({
-            chatId: 1035451470,
-            username: 'Amed152',
-            queryId: 'AAFOvLc9AAAAAE68tz3ynEhS'
-        })
-        //  authUser({
-        //     chatId: tg?.initDataUnsafe?.user?.id,
-        //     username: tg?.initDataUnsafe?.user?.username,
-        //     queryId: tg?.initDataUnsafe?.query_id
+        // authUser({
+        //     chatId: 1035451470,
+        //     username: 'Amed152',
+        //     queryId: 'AAFOvLc9AAAAAE68tz3ynEhS'
         // })
+         authUser({
+            chatId: tg?.initDataUnsafe?.user?.id,
+            username: tg?.initDataUnsafe?.user?.username,
+            queryId: tg?.initDataUnsafe?.query_id
+        })
     }, []);
     useEffect(() => {
         if (data) {
