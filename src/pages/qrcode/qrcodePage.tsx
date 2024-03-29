0e171099aa7bfc4565572ab1e959bda1558a1332
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-29 19:08:30 +0300

    commit

--- src/pages/qrcode/qrcodePage.tsx
+++ src/pages/qrcode/qrcodePage.tsx
@@ -7,7 +7,7 @@ const QrcodePage = () => {
     return (
         <MainLayout heading={''}>
             <div className={classes.qrcode}>
-                <h2 className={'mb-4'}>Для зааза перейдите по ссылке или сканируйте QR код</h2>
+                <h2 className={'mb-4'}>Для заказа перейдите по ссылке или сканируйте QR код</h2>
                 <a className={classes.link} href="https://t.me/javatest92_bot">Перейти по ссылке</a>
                 <img className={classes.image} src={`${process.env.REACT_APP_API_URL}qrcode.png`} alt="qrcode"/>
             </div>
